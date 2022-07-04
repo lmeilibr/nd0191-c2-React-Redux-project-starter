ab0f0ce853ea10ddf739b700afe99ece40305ebc
Leandro Meili <leandro.meili@gmail.com>
2022-07-03 21:31:47 -0300

    add leaderboard

--- poll-app/src/components/Leaderboard.js
+++ poll-app/src/components/Leaderboard.js
@@ -1,12 +1,37 @@
 import Navigation from "./Navigation";
+import {connect} from "react-redux";
+import Question from "./Question";
 
-function Leaderboard() {
+const Leaderboard = (props) => {
     return (
         <>
             <Navigation/>
             <h1>Leaderboard</h1>
+            {console.log(props.usersKeys)}
+            <table>
+                <thead>
+                <th>Users</th>
+                <th>Answered</th>
+                <th>Created</th>
+                </thead>
+                <tbody>
+                 {props.usersKeys.map((user)=>(
+                     // console.log(user)
+                    <tr key={user.id}>
+                        <td>{props.users[user].name}</td>
+                        <td>{Object.keys(props.users[user].answers).length}</td>
+                        <td>{props.users[user].questions.length}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
         </>
     )
 }
 
-export default Leaderboard
+const mapStateToProps = ({users, questions}) => ({
+    usersKeys: Object.keys(users),
+    questions,
+    users
+})
+export default connect(mapStateToProps)(Leaderboard);
