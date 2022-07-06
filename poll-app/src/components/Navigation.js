ee3124c1569a280421f736dbda496239132ac14a
Leandro Meili <leandro.meili@gmail.com>
2022-07-06 00:14:59 -0300

    handle logout

--- poll-app/src/components/Navigation.js
+++ poll-app/src/components/Navigation.js
@@ -1,18 +1,28 @@
 import {Link} from "react-router-dom";
+import {connect} from "react-redux";
+import {setAuthedUser} from "../actions/authedUser";
 
-function Navigation(){
+function Navigation(props) {
+    const handleLogout = () => {
+        props.dispatch(setAuthedUser(null))
+    }
     return (
         <nav>
             <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                    <Link to="/leaderboard">Leaderboard</Link>
-                    <Link to="/add">New</Link>
-                    <Link to="/logout">Logout</Link>
-                </li>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/leaderboard">Leaderboard</Link></li>
+                <li><Link to="/add">New</Link></li>
+                <li>{props.authedUser}</li>
+                <li onClick={handleLogout}><Link to="/">Logout</Link></li>
             </ul>
         </nav>
     )
 }
 
-export default Navigation
+const mapStateToProps = ({authedUser}) => (
+    {
+        authedUser,
+    }
+)
+
+export default connect(mapStateToProps)(Navigation)
